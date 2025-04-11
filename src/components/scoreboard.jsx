54f23344336930e0be3c4f4af68ea3cef3df9ed5
Alejandro Diaz <contact@alejandrodizor.com>
2025-04-11 01:30:29 -0500

    header updated

---
+++ src/components/scoreboard.jsx
@@ -0,0 +1,8 @@
+
+export const Scoreboard = () => {
+  return (
+    <div className="flex justify-center border-3 border-black mt-12 p-10 bg-amber-100/20">
+        <h3 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl tracking-tighter text-balance text-center">Antiguo Testamento</h3>
+    </div>
+  )
+}
