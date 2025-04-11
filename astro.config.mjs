b04fa6dd82e9611a91a71e9a61d64ff5cf2c312f
Alejandro Diaz <contact@alejandrodizor.com>
2025-04-11 18:42:52 -0500

    Astro config updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,10 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    
   },
+  output: 'server',
 
   integrations: [react()],
   adapter: cloudflare()
